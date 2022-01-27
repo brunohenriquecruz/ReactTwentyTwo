3453a3cb0aa435f518e045c32d6e4f9df24d5f07
Bruno Henrique da Cruz <brunohenriquecruz@yahoo.com.br>
2022-01-26 23:06:12 -0300

    React 2022 by RocketSeat

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { Tweet } from "./components/Tweet";
+
+export default function App() {
+  const [tweets, setTweets] = useState<string[]>([
+    'Tweet 1',
+    'Tweet 2',
+    'Tweet 3',
+    'Tweet 4',
+  ])
+
+  function createTweet(){
+    setTweets([...tweets, 'Tweet 5'])
+  }
+
+  return(
+    <div>
+      {tweets.map(tweet => {
+        return <Tweet text= {tweet} />
+      })}
+
+      <button onClick={createTweet}>Adicionar tweet</button>
+    </div>
+  );
+}
